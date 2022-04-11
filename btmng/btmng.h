9a9434384c29dab585ec4ef665296e94507956fc
Cromulent74 <ilya.fomko03@mail.ru>
2022-04-11 18:00:25 +0500

    print only sets with same set of numbers

---
+++ btmng/btmng.h
@@ -0,0 +1,16 @@
+//
+// Created by Iluha on 11.04.2022.
+//
+
+#pragma once
+#ifndef INDIVIDUAL_TASK_HOMEWORK_BTMNG_H
+#define INDIVIDUAL_TASK_HOMEWORK_BTMNG_H
+
+
+class btmng {
+    int bits;
+
+};
+
+
+#endif //INDIVIDUAL_TASK_HOMEWORK_BTMNG_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+void print(uint32_t bitArray, int n) {
+    for (int i = 0; i < n; i++)
+        if (bitArray & (1 << i))
+            std::cout << i << " ";
+}
+
 int main() {
-    int n, k, res = 0, count = 0;
+    int n, k, count = 0;
+    uint32_t res = 0;
 
     std::cout << "Enter size of set: ";
     std::cin >> n;
@@ -16,11 +23,52 @@ int main() {
 
     while (res < (1 << n)) {
         if (count == 0) {
-            std::cout << "{ ";
+            // Creating set of numbers for all set of size k
+
+            uint32_t numSet = 0;
+
             for (int j = 0; j < n; j++)
-                if (res & (1 << j))
-                    std::cout << j << " ";
-            std::cout << "}\n";
+                if (res & (1 << j)) {
+                    int x = j;
+
+                    while (x > 0) {
+                        numSet |= (1 << (x % 10));
+                        x /= 10;
+                    }
+
+                    break;
+                }
+
+            // Checking for full set intersection of number everything number from set size k
+
+            bool isEqualSets = true;
+
+            for (int j = 0; j < n; j++)
+                if (res & (1 << j)) {
+                    int x = j;
+
+                    uint32_t tempSet = 0;
+
+                    while (x > 0) {
+                        tempSet |= (1 << (x % 10));
+                        x /= 10;
+                    }
+
+                    if (tempSet != numSet) {
+                        isEqualSets = false;
+                        break;
+                    }
+                }
+
+            // print numbers of set
+
+            if (isEqualSets) {
+                std::cout << "{ ";
+                print(res, n);
+                std::cout << "}\n";
+            }
+
+            // Delete numbers in set and right shift
 
             i = 0;
 
@@ -37,7 +85,7 @@ int main() {
 
             res |= (1 << i);
             count--;
-        } else {
+        } else { // Add numbers in the set
             i = 0;
 
             while (count != 0) {
